a5cad5dc28fb5705128fd1f1e7998f8b394c4c27
hbuckle2 <47196713+hbuckle2@users.noreply.github.com>
2022-04-19 03:18:58 -0400

    bug fix and logic clean

--- TimerLogic.py
+++ TimerLogic.py
@@ -1,59 +1,62 @@
-from threading import Timer
+# When setting the time only use two digit inputs
+
 from tkinter import *
 import time
-from winsound import PlaySound
 
 app_window = Tk()
-app_window.geometry('500x500')
-app_window.resizable(0,0)
-app_window.config(bg = 'blanched almod')
+app_window.geometry('500x400')
+app_window.resizable(0, 0)
+app_window.config(bg='black')
 app_window.title('Horace Countdown Clock And Timer')
-Label(app_window, text= 'Countdown Clock and Timer', font = 'arial 20 bold', bg = 'papaya whip').pack()
+Label(app_window, text='Countdown Clock and Timer', font='arial 20 bold', bg='green').pack()
 
-Label(app_window, font = 'arial 15 bold', text = 'current time :', bg = 'papaya whip').place(x=40, y=70)
+Label(app_window, font='arial 15 bold', text='Current Time :', bg='green').place(x=59, y=70)
+
 
 def clock():
     clock_time = time.strftime('%H:%M:%S %p')
-    curr_time.config(text = clock_time)
-    curr_time.after(1000,clock)
+    curr_time.config(text=clock_time)
+    curr_time.after(1000, clock)
 
-curr_time =Label(app_window, font ='arial 15 bold', text = '', fg = 'gray25' ,bg ='papaya whip')
-curr_time.place(x = 190 , y = 70)
+
+curr_time = Label(app_window, font='arial 15 bold', text='', fg='gray25', bg='red')
+curr_time.place(x=200, y=70)
 clock()
 
-sec = StringVar()
-Entry(app_window, textvariable = sec, width = 2, font = 'arial 12').place(x=250, y=155)
-sec.set('00')
-mins= StringVar()
-Entry(app_window, textvariable = mins, width =2, font = 'arial 12').place(x=225, y=155)
-mins.set('00')
-hrs= StringVar()
-Entry(app_window, textvariable = hrs, width =2, font = 'arial 12').place(x=200, y=155)
-hrs.set('00')
+seconds = StringVar()
+Entry(app_window, textvariable=seconds, width=2, font='arial 12').place(x=250, y=155)
+seconds.set('00')
+minutes = StringVar()
+Entry(app_window, textvariable=minutes, width=2, font='arial 12').place(x=225, y=155)
+minutes.set('00')
+hours = StringVar()
+Entry(app_window, textvariable=hours, width=2, font='arial 12').place(x=200, y=155)
+hours.set('00')
+
 
 def countdown():
-    times = int(hrs.get())*3600+ int(mins.get())*60 + int(sec.get())
+    times = int(hours.get()) * 3600 + int(hours.get()) * 60 + int(seconds.get())
     while times > -1:
-        minute,second = (times // 60 , times % 60)
-        
+        minute, second = (times // 60, times % 60)
+
         hour = 0
         if minute > 60:
-            hour , minute = (minute // 60 , minute % 60)
-      
-        sec.set(second)
-        mins.set(minute)
-        hrs.set(hour)
-   
+            hour, minute = (minute // 60, minute % 60)
+
+        seconds.set(second)
+        minutes.set(minute)
+        hours.set(hour)
+
         app_window.update()
         time.sleep(1)
-        if(times == 0):
-            #playsound('Loud_Alarm_Clock_Buzzer.mp3')
-            sec.set('00')
-            mins.set('00')
-            hrs.set('00')
+        if times == 0:
+            seconds.set('00')
+            minutes.set('00')
+            hours.set('00')
         times -= 1
-        
-        
-Label(app_window, font ='arial 15 bold', text = 'set the time',   bg ='papaya whip').place(x = 40 ,y = 150)
-Button(app_window, text='START', bd ='5', command = countdown, bg = 'antique white', font = 'arial 10 bold').place(x=150, y=210)
+
+
+Label(app_window, font='arial 15 bold', text='Set Timer', bg='green').place(x=59, y=150)
+Button(app_window, text='START TIMER', bd='5', command=countdown, bg='red', font='arial 10 bold').place(x=150,
+                                                                                                            y=210)
 app_window.mainloop()
